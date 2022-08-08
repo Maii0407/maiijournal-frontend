151e9d3d0c06767750a5be1ee4dc94f65d4a8012
Akmal Izuddin <izuddin0407@gmail.com>
2022-08-08 11:06:02 +0800

    minor additions: completed the authentication logic in the frontend of the app

--- src/components/admin/Admin.js
+++ src/components/admin/Admin.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Admin = () => {
+  const [ userIDState, setUserIDState ] = useState('');
+  const [ passwordState, setPasswordState ] = useState('');
+
+  const login = async (e) => {
+    e.preventDefault();
+
+    const url = 'http://localhost:4000/admin/superSecretAdminLogin';
+    const formData = {
+      userID: userIDState,
+      password: passwordState
+    };
+
+    try {
+      const response = await fetch( url, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify( formData ),
+      });
+      const data = await response.json();
+      console.log( 'Success:', data );
+      setUserIDState('');
+      setPasswordState('');
+    }
+    catch( err ) {
+      console.log( 'Error:', err );
+    }
+  };
+
   return (
-    <form className='Admin-form'>
+    <form className='Admin-form' onSubmit={ login }>
       <label htmlFor='userID'>User ID:</label>
-      <input type='text' id='userID' name='userID' placeholder='Enter User ID'/>
+      <input type='text' id='userID' name='userID'
+        placeholder='Enter User ID' onChange={ e => setUserIDState( e.target.value ) }
+        value={ userIDState }
+      />
       <label htmlFor='password'>Password:</label>
-      <input type='password' id='password' name='password' placeholder='Enter User Password'/>
+      <input type='password' id='password' name='password'
+        placeholder='Enter User Password' onChange={ e => setPasswordState( e.target.value ) }
+        value={ passwordState }
+      />
       <button type='submit'>Submit</button>
     </form>
   );
